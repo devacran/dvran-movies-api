6b7cc3476ad7fb72cfb9b009a563166edb3711eb
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2022-07-16 13:31:09 -0500

    fix mongo db config

--- config/index.js
+++ config/index.js
@@ -7,6 +7,7 @@ const config = {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
   dbName: process.env.DB_NAME,
 };
 
